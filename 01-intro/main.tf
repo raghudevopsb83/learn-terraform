2c9d35c493d3e5c1b063d30641b4854dd459330d
SVR Kalluru <raghuk.vit@gmail.com>
2025-02-24 07:44:36 +0530

    Training Code

--- 01-intro/main.tf
+++ 01-intro/main.tf
@@ -5,3 +5,20 @@ resource "local_file" "foo" {
   filename = "/tmp/foo.bar"
 }
 
+# Normal Variable
+variable "x" {
+  default = 10
+}
+
+
+
+
+## Print variable on output
+output "x" {
+  value = var.x
+}
+
+# if our value is just variable then var.x , if it is combination of strings then "${var.x}"
+output "x1" {
+  value = "Value of x - ${var.x}"
+}
