1366c2252bea2d84e96ea8307de85544adc13d18
Raghu K <raghuk.vit@gmail.com>
2025-02-27 07:46:11 +0530

    RoboShop Code

--- 05-conditions/main.tf
+++ 05-conditions/main.tf
@@ -7,3 +7,13 @@ variable "content" {
   default = null
 }
 
+# condition is all about what value to give to a argument.
+# Now we can use this as an advantage and decide whether we can run this resource or not using a count loop
+
+resource "null_resource" "test" {
+  count = 0
+}
+
+
+
+
