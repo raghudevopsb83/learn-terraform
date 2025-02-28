69e17dedc5ebc942204833b4891dee2f6cc84531
Raghu K <raghuk.vit@gmail.com>
2025-02-28 08:29:15 +0530

    RoboShop Code

---
+++ 07-vault/main.tf
@@ -0,0 +1,18 @@
+provider "vault" {
+  address = "http://vault-internal.rdevopsb83.online:8200"
+  token = var.vault_token
+}
+
+variable "vault_token" {}
+
+data "vault_kv_secret" "secret_data" {
+  path = "test/data/demo"
+}
+
+# resource "local_file" "test" {
+#   filename = "/tmp/1"
+#   content = data.vault_kv_secret.secret_data.data
+# }
+
+
+
